1d4c93d5319dabe878f4f46e8b0176d2e2e06302
kevin <kevinmbari600@gmail.com>
2024-01-09 18:16:36 +0300

    assignment done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import  Flask
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return f'<p>Printed String: {param}</p>'
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = '\n'.join(str(i) for i in range(param + 1))
+    return f'<pre>{numbers}</pre>'
+
+@app.route('/math/<int:num1><operation><int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    
+    return f'<p>Result of {num1} {operation} {num2} is {result}</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
